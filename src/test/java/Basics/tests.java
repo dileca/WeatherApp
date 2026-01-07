17d471c0c722117da5fef3695f6b3a1f6f6455b2
DianaL <dianaleca1@gmail.com>
2026-01-07 15:36:44 +0200

    added update and delete

--- src/test/java/Basics/tests.java
+++ src/test/java/Basics/tests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class tests {
 
     static String baseURL = "http://api.openweathermap.org";
-    static String authToken = "f31404aed7e27d3bb39bb80ceea07911" ;
+    static String authToken = "f31404aed7e27d3bb39bb80ceea07911";
     static String stationID;
 
     @Test(priority = 1)
@@ -33,11 +33,11 @@ public class tests {
                 .log().all()
                 .post().prettyPeek();
 
-       // validate data returned for each response field
-       // .body("external_id", equalTo("{{external_id}}"))//.body("name", equalTo("{{station_name}}"))
-       // .body("latitude", equalTo(37.76f))
-       // .body("longitude", equalTo(-122.43f))
-       // .body("altitude", equalTo(150));
+        // validate data returned for each response field
+        // .body("external_id", equalTo("{{external_id}}"))//.body("name", equalTo("{{station_name}}"))
+        // .body("latitude", equalTo(37.76f))
+        // .body("longitude", equalTo(-122.43f))
+        // .body("altitude", equalTo(150));
 
         //Extract station id for further use
         stationID = responseBody.jsonPath().getString("ID");
@@ -120,11 +120,56 @@ public class tests {
         int actualStatusCode = responseBody.getStatusCode();
         Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
     }
+    @Test(priority = 5)
+    public void updateLongitudeAndLatitude() {
+        String path = "/data/3.0/stations/{stationID}";
+        String payload = " {\n" +
+                "  \"external_id\": \"{{external_id}}\",\n" +
+                "  \"name\": \"{{station_name}}\",\n" +
+                "  \"latitude\": 41.62,\n" +
+                "  \"longitude\": -115.43,\n" +
+                "  \"altitude\": 150\n" +
+                "}";
 
+        Response responseBody = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath(path)
+                .header("Content-Type", "application/json")
+                .pathParam("stationID", stationID)
+                .queryParam("appid", authToken)
+                .body(payload)
+                .log().all()
+                .put().prettyPeek();
 
+        System.out.println(responseBody.getBody().asString());
 
+        int actualStatusCode = responseBody.getStatusCode();
+        Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+    }
 
+    @Test(priority = 6)
+    public void deleteWeatherStation() {
+
+        String path = "/data/3.0/stations/{stationID}";
+
+        Response responseBody = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath(path)
+                .header("Content-Type", "application/json")
+                .pathParam("stationID", stationID)
+                .queryParam("appid", authToken)
+                .log().all()
+                .delete().prettyPeek();
+
+        System.out.println(responseBody.getBody().asString());
+
+        int actualStatusCode = responseBody.getStatusCode();
+        Assert.assertEquals(actualStatusCode, 204, "Status code should be 204");
+    }
 }
+
+
+
 
 
 
